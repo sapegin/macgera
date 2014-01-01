6c864b41e6f1af09d32f7d1ac2a366235e391b4c
Mihail Yakimenko <macgera@gmail.com>
2014-01-01 23:27:10 +0200

    rm idea with big images on background

--- markup/js/background.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    var num = (Math.floor(Math.random()*4));
-    var array = ['one', 'two', 'three', 'four'];
-    var elem = document.getElementById('main');
-    elem.classList.add(array[num]);
-});
